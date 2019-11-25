df9aceb377e6b370020022882811229b43d6d741
sharaquss <przjablonski@gmail.com>
2019-11-25 18:12:06 +0100

    Initial Commit

---
+++ Casimir/Casimir.h
@@ -0,0 +1,19 @@
+//
+//  Casimir.h
+//  Casimir
+//
+//  Created by Przemyslaw Jablonski on 25/11/2019.
+//  Copyright © 2019 com.szparag.ios. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Casimir.
+FOUNDATION_EXPORT double CasimirVersionNumber;
+
+//! Project version string for Casimir.
+FOUNDATION_EXPORT const unsigned char CasimirVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Casimir/PublicHeader.h>
+
+
