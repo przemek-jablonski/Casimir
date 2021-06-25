5d75ceb907d3c2715d651819897f10956216dbe4
Przemysław Jabłoński <przjablonski@gmail.com>
2021-06-25 19:49:08 +0200

    tests for math functions and array conversiont

--- Sources/Casimir/Foundation/Array/Array+removeDuplicates.swift
+++ Sources/Casimir/Foundation/Array/Array+removeDuplicates.swift
@@ -1,18 +1,10 @@
 public extension Array where Element: Hashable {
     func removeDuplicates() -> [Element] {
-        var buffer = Array()
-        var added = Set<Element>()
-        for elem in self {
-            if !added.contains(elem) {
-                buffer.append(elem)
-                added.insert(elem)
-            }
-        }
-        return buffer
+        Casimir.removeDuplicates(source: self)
     }
 }
 
-private func removeDuplicates<S: Sequence, T: Hashable>(source: S) -> [T] where S.Iterator.Element == T {
+public func removeDuplicates<S: Sequence, T: Hashable>(source: S) -> [T] where S.Iterator.Element == T {
     var buffer = [T]()
     var added = Set<T>()
     for elem in source {
--- Sources/Casimir/Foundation/Clamp.swift
+++ Sources/Casimir/Foundation/Clamp.swift
@@ -1,21 +1,11 @@
-//
-//  Clamp.swift
-//  Clamp
-//
-//  Created by Daniel Clelland on 6/12/15.
-//  Copyright © 2015 Daniel Clelland. All rights reserved.
-//
+//  Originally Created by Daniel Clelland
 
 import Foundation
 import CoreGraphics
 
-// MARK: Clampable protocol
-
 public protocol Clampable {
     func clamp(min: Self, max: Self) -> Self
 }
-
-// MARK: Clampable global functions
 
 /**
 Clamps the value between an lower and upper bound.
@@ -28,54 +18,40 @@ Clamps the value between an lower and upper bound.
 */
 
 public func clamp<T: Clampable>(_ value: T, min: T, max: T) -> T {
-    return value.clamp(min: min, max: max)
+    value.clamp(min: min, max: max)
 }
 
-// MARK: Clampable implementations
-
 extension Double: Clampable {
-    
-    /// Clamp
     public func clamp(min: Double, max: Double) -> Double {
-        return Swift.min(Swift.max(self, Swift.min(min, max)), Swift.max(min, max))
+        Swift.min(Swift.max(self, Swift.min(min, max)), Swift.max(min, max))
     }
     
 }
 
 extension Float: Clampable {
-    
-    /// Clamp
     public func clamp(min: Float, max: Float) -> Float {
-        return Swift.min(Swift.max(self, Swift.min(min, max)), Swift.max(min, max))
+        Swift.min(Swift.max(self, Swift.min(min, max)), Swift.max(min, max))
     }
     
 }
 
 extension CGFloat: Clampable {
-    
-    /// Clamp
     public func clamp(min: CGFloat, max: CGFloat) -> CGFloat {
-        return Swift.min(Swift.max(self, Swift.min(min, max)), Swift.max(min, max))
+        Swift.min(Swift.max(self, Swift.min(min, max)), Swift.max(min, max))
     }
     
 }
 
 extension CGPoint: Clampable {
-    
-    /// Clamp
     public func clamp(min: CGPoint, max: CGPoint) -> CGPoint {
         let x = self.x.clamp(min: min.x, max: max.x)
         let y = self.y.clamp(min: min.y, max: max.y)
         return CGPoint(x: x, y: y)
     }
-    
 }
 
-// MARK: CGPoint clampable extension
-
 public extension CGPoint {
-    /// Clamp to a rect
     func clamp(rect: CGRect) -> CGPoint {
-        return clamp(min: rect.min, max: rect.max)
+        clamp(min: rect.min, max: rect.max)
     }
 }
