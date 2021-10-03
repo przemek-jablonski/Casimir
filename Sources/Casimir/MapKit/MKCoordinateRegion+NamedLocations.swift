e30328c25f410f4d4ce2e7535be7b1e7af55f562
Przemysław Jabłoński <przjablonski@gmail.com>
2021-10-03 17:39:09 +0200

    Added MapKit extensions with NamedLocations

---
+++ Sources/Casimir/MapKit/MKCoordinateRegion+NamedLocations.swift
@@ -0,0 +1,17 @@
+import MapKit
+
+public extension MKCoordinateRegion {
+    static var petersburg: MKCoordinateRegion { MKCoordinateRegion(center: .petersburg, latitudinalMeters: 62, longitudinalMeters: 23) }
+    static var tokyo: MKCoordinateRegion { MKCoordinateRegion(center: .tokyo, latitudinalMeters: 39, longitudinalMeters: 26) }
+    static var cupertino: MKCoordinateRegion { MKCoordinateRegion(center: .cupertino, latitudinalMeters: 60, longitudinalMeters: 55) }
+    static var nyc: MKCoordinateRegion { MKCoordinateRegion(center: .nyc, latitudinalMeters: 76, longitudinalMeters: 39) }
+    static var santiago: MKCoordinateRegion { MKCoordinateRegion(center: .santiago, latitudinalMeters: 30, longitudinalMeters: 32) }
+    static var reykjavik: MKCoordinateRegion { MKCoordinateRegion(center: .reykjavik, latitudinalMeters: 25, longitudinalMeters: 41) }
+}
+
+public extension MKCoordinateRegion {
+    static var random: MKCoordinateRegion {
+        [.petersburg, .tokyo, .cupertino, .nyc, .santiago, .reykjavik].randomElement() ?? .petersburg
+    }
+}
+
