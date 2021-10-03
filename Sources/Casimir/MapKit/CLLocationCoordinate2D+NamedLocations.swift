871911fad130778d19c2df0cf68a367a333852df
Przemysław Jabłoński <przjablonski@gmail.com>
2021-10-03 17:39:36 +0200

    Added remaining NamedLocations fo the CLLocationCoordinate2D extension

--- Sources/Casimir/MapKit/CLLocationCoordinate2D+NamedLocations.swift
+++ Sources/Casimir/MapKit/CLLocationCoordinate2D+NamedLocations.swift
@@ -1,3 +1,10 @@
-//
+import CoreLocation
 
-import Foundation
+public extension CLLocationCoordinate2D {
+    static var petersburg: CLLocationCoordinate2D { CLLocationCoordinate2D(latitude: 59.56, longitude: 30.18) }
+    static var tokyo: CLLocationCoordinate2D { CLLocationCoordinate2D(latitude: 35.41, longitude: 139.41) }
+    static var cupertino: CLLocationCoordinate2D { CLLocationCoordinate2D(latitude: 37.19, longitude: 122.01) }
+    static var nyc: CLLocationCoordinate2D { CLLocationCoordinate2D(latitude: 40.42, longitude: 74.00) }
+    static var santiago: CLLocationCoordinate2D { CLLocationCoordinate2D(latitude: 33.27, longitude: 70.40) }
+    static var reykjavik: CLLocationCoordinate2D { CLLocationCoordinate2D(latitude: 64.08, longitude: 21.56) }
+}
