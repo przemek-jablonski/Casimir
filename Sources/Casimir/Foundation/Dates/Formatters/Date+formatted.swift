1c3c3c7595ff2d2471a20e4529d903e6e616e036
przemek-jablonski <przemek.jablonski@protonmail.com>
2022-02-03 12:48:03 +0100

    Fixing availability for Date+formatted

--- Sources/Casimir/Foundation/Dates/Formatters/Date+formatted.swift
+++ Sources/Casimir/Foundation/Dates/Formatters/Date+formatted.swift
@@ -1,20 +1,24 @@
 #if os(iOS)
 import Foundation
 
-@available(iOS 15.0, *)
+@available(iOS, introduced: 15.0)
 public extension Date {
+    @available(iOS 15.0, *)
     var formattedCompleteDateOnly: String {
         formatted(date: .complete, time: .omitted)
     }
     
+    @available(iOS 15.0, *)
     var formattedCompleteTimeOnly: String {
         formatted(date: .omitted, time: .complete)
     }
     
+    @available(iOS 15.0, *)
     var formattedCompleteDateTime: String {
         formatted(date: .complete, time: .complete)
     }
     
+    @available(iOS 15.0, *)
     var formattedShortDateTime: String {
         formatted(date: .abbreviated, time: .shortened)
     }
