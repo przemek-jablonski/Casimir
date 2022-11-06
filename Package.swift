2c8e329610b04225af2d9ac78319b9e267a85f09
Przemek Jabłoński <przemek.jablonski@protonmail.com>
2022-11-06 23:52:56 +0100

    Downgrading platforms to 14 as minimum (#20)
    * Downgrading platforms to 14 as minimum
    
    * Expanded CI runs by adding Xcode 12.4 on macOS-11
    
    * Downgraded swift to 5.4.2, updated xcode to 12.5.1
    
    * Downgraded swift version to 5.4.0
    
    * swift-tools-version to 5.5.0
    
    * CasimirPlayground
    
    * Added Casimir as local
    
    * Linked Casimir and Testimir to targets in Playground
    
    * Removed Playground

--- Package.swift
+++ Package.swift
@@ -1,15 +1,14 @@
 // swift-tools-version:5.5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Casimir",
     platforms: [
-        .iOS(.v15),
-        .watchOS(.v8),
-        .tvOS(.v15),
-        .macOS(.v12)
+        .iOS(.v14),
+        .watchOS(.v7),
+        .tvOS(.v14),
+        .macOS(.v11)
     ],
     products: [
         .library(
