e6b1bd5b30a81822dac279c5d02549f8fd0aa209
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-02-05 19:24:30 +0100

    Removed UI dependencies

--- Package.swift
+++ Package.swift
@@ -28,8 +28,6 @@ let package = Package(
         .package(url: "https://github.com/CombineCommunity/CombineExt", .exact("1.8.0")),
         .package(url: "https://github.com/tcldr/Entwine.git", .exact("0.9.1")),
         .package(url: "https://github.com/SwiftyBeaver/SwiftyBeaver.git", .exact("1.9.6")),
-        .package(url: "https://github.com/SwiftUIX/SwiftUIX.git", .exact("0.1.3")),
-        .package(url: "https://github.com/SFSafeSymbols/SFSafeSymbols.git", .exact("4.0.0"))
     ],
     targets: [
         .target(
@@ -37,9 +35,7 @@ let package = Package(
             dependencies: [
                 .product(name: "CombineExt", package: "CombineExt"),
                 .product(name: "Entwine", package: "Entwine"),
-                .product(name: "SFSafeSymbols", package: "SFSafeSymbols"),
                 .product(name: "SwiftyBeaver", package: "SwiftyBeaver"),
-                .product(name: "SwiftUIX", package: "SwiftUIX")
             ]
         ),
         .target(
