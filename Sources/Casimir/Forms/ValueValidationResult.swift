19d71c0622aca9e907ce263a7d9e0208bb9c2cc9
przemek-jablonski <przemek.jablonski@protonmail.com>
2023-06-10 18:54:22 +0200

    Removed redundant Forms module

--- Sources/Casimir/Forms/ValueValidationResult.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ValueValidationResult.swift
-//  automemo-ios
-//
-//  Created by Przemyslaw Jablonski on 29/07/2020.
-//  Copyright © 2020 com.szparag.ios. All rights reserved.
-//
-
-import Foundation
